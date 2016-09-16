be8dc086d709b4c5ae2da32570796e46ac72c5d1
Darren Reid <darren.j.reid@gmail.com>
2016-09-16 11:14:24 +1000

    Auto fix double bracket links until they migrated

---
+++ docs/js/fix-double-bracket-links.js
@@ -0,0 +1,26 @@
+(function () {
+    "use strict";
+    String.prototype.replaceAll = function(search, replacement) {
+        var target = this;
+        return target.split(search).join(replacement);
+    };
+
+    function fixDoubleBracketLinks(cssSelector) {
+        var sidebarText = $(cssSelector)[0].innerHTML;
+        var matches = sidebarText.match(/\[\[(.+?)\]\]/g);
+        var result = sidebarText;
+        if(matches != null) {
+            for(var i = 0; i < matches.length; i++) {
+                var text = matches[i].replaceAll('[[','').replaceAll(']]','');
+                result = result.replace(matches[i],
+                    '<a href="documentation/' +
+                    text.replaceAll(' ','-') +
+                    '.html">' + text + '</a>')
+            }
+            $(cssSelector).html(result);
+        }
+    }
+
+    fixDoubleBracketLinks('#page-content-wrapper');
+    fixDoubleBracketLinks('#sidebar-wrapper');
+})();
